db394027ed21459db4ab51b6180bce3ed8312119
Miguel-Restrepo <miguel.1701911153@ucaldas.edu.co>
2021-04-14 07:57:45 -0500

    pagina maestra

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BarraNavegacionSuperiorComponent } from './publico/paginaMaestra/barra-navegacion-superior/barra-navegacion-superior.component';
+import { BarraNavegacionLateralComponent } from './publico/paginaMaestra/barra-navegacion-lateral/barra-navegacion-lateral.component';
+import { PiePaginaComponent } from './publico/paginaMaestra/pie-pagina/pie-pagina.component';
+import { InicioComponent } from './publico/inicio/inicio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BarraNavegacionSuperiorComponent,
+    BarraNavegacionLateralComponent,
+    PiePaginaComponent,
+    InicioComponent
   ],
   imports: [
     BrowserModule,
